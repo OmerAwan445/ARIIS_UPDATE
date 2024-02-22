8ae7665d4ba34061697cdf3a1f7cf16b76dda3e1
OmerAwan445 <omerawan445@gmail.com>
2024-02-23 00:23:49 +0500

    Designed /analytics Route

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@
 import { Header } from '@/components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
-
 import { Sidebar } from '@/components/Sidebar/Sidebar';
 import "leaflet/dist/leaflet.css";
 import Script from 'next/script';
@@ -17,18 +16,18 @@ export default function RootLayout({ children }) {
     <html>
 
       <body>
-          <div className='w-100'>
-            {/* <!-- partial:partials/_navbar.html --> */}
-            <Header />
-            {/* <!-- partial --> */}
-            <div className="container-fluid page-body-wrapper">
-              <Sidebar />
-              {children}
 
+            <div className='w-100'>
+              {/* <!-- partial:partials/_navbar.html --> */}
+              <Header />
+              {/* <!-- partial --> */}
+              <div className="container-fluid page-body-wrapper">
+                <Sidebar />
+                {children}
+              </div>
+              {/* <!-- page-body-wrapper ends --> */}
             </div>
-            {/* <!-- page-body-wrapper ends --> */}
-          </div>
-          {/* <!-- container-scroller --> */}
+        {/* <!-- container-scroller --> */}
         {/* <!-- plugins:js --> */}
         <Script src="/js/jquery.js" strategy="afterInteractive" />
         <Script src="/js/vendor.bundle.base.js" strategy="afterInteractive" />
