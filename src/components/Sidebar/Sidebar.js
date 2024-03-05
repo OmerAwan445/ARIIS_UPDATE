2b8f3c99c8015812cb26b5060ae53c469a9e1026
OmerAwan445 <omerawan445@gmail.com>
2024-03-06 02:11:23 +0500

    Set Side Bar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,35 +4,42 @@ import { PiPathBold } from "react-icons/pi";
 import { GoGraph } from "react-icons/go";
 import { IoIosSettings } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
+import Link from 'next/link';
 export const Sidebar = () => {
   return (
-        <nav className="sidebar sidebar-offcanvas" id="sidebar">
-          <ul className="nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-              <FaRegMap  className='sidebar-icon'/>
-                <span className="menu-title">Map</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-              <PiPathBold  className='sidebar-icon'/>
-                <span className="menu-title">Path</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-              <GoGraph  className='sidebar-icon'/>
-                <span className="menu-title">Graph</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-              <IoIosSettings  className='sidebar-icon'/>
-                <span className="menu-title">Setting</span>
-              </a>
-            </li>
-            {/* <li className="nav-item">
+    <nav className="sidebar sidebar-offcanvas" id="sidebar">
+      <ul className="nav">
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            <FaRegMap className='sidebar-icon' />
+            <span className="menu-title">Map</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            <PiPathBold className='sidebar-icon' />
+            <span className="menu-title">Path</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            <GoGraph className='sidebar-icon' />
+            <span className="menu-title">Graph</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            <IoIosSettings className='sidebar-icon' />
+            <span className="menu-title">Setting</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <Link href="/aris-run"
+            className='nav-link rounded py-3 border-0'>
+            <span className='d-block'>Aris Run View</span>
+          </Link>
+        </li>
+        {/* <li className="nav-item">
               <a className="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
                 <i className="icon-layout menu-icon"></i>
                 <span className="menu-title">UI Elements</span>
@@ -46,12 +53,12 @@ export const Sidebar = () => {
                 </ul>
               </div>
             </li> */}
-            <li>
-              <button className="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-               <span className='nav-link'><RxHamburgerMenu className='sidebar-icon' /></span>
-              </button>
-            </li>
-          </ul>
-        </nav>
+        <li>
+          <button className="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span className='nav-link'><RxHamburgerMenu className='sidebar-icon' /></span>
+          </button>
+        </li>
+      </ul>
+    </nav>
   )
 }
