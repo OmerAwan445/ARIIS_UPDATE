2d3bb382cb47e4dba8418cfb4bdc31c7f42db6f4
Muhammad Umer <omerawan445@gmail.com>
2024-03-16 01:41:00 +0500

    Made ExportDataToExcel Component which will export provided Data with provided fileName.

--- src/components/Table/CustomTable.js
+++ src/components/Table/CustomTable.js
@@ -1,31 +1,37 @@
 import React from "react";
 import { Table } from "react-bootstrap";
+import ExportDataToExcel from "../common/ExportDataToExcel";
 
-export const CustomTable = ({ columns, rows ,title}) => {
+export const CustomTable = ({ columns, rows, title, isExportData }) => {
   return (
     <>
-    <div>
-        <h3 className="fw-fw-lighter">{title}</h3>
+      <div>
+        <div className="d-flex justify-content-between align-items-center">
+          <h3 className="fw-fw-lighter">{title}</h3>
+          {isExportData &&
+            <ExportDataToExcel fileName={"section-details"} exportData={[columns, ...rows]} />
+        }
+        </div>
         <Table className='custom-table' responsive="sm">
           <thead>
-          <tr>
-          {columns?.map((column, index) => (
-            <th key={index}>{column}</th>
-          ))}
-        </tr>
-          </thead>
-          <tbody>
-          {rows?.map((rowData, rowIndex) => (
-            <tr key={rowIndex}>
-              {rowData.map((cellData, cellIndex) => (
-                <td key={cellIndex}>{cellData}</td>
+            <tr>
+              {columns?.map((column, index) => (
+                <th key={index}>{column}</th>
               ))}
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {rows?.map((rowData, rowIndex) => (
+              <tr key={rowIndex}>
+                {rowData.map((cellData, cellIndex) => (
+                  <td key={cellIndex}>{cellData}</td>
+                ))}
+              </tr>
+            ))}
           </tbody>
         </Table>
       </div>
-    </> 
-  
+    </>
+
   );
 };
