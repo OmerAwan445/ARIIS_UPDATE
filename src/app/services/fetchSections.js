5934270f0dc1af40abb661433c6f0b2de44e5c72
Muhammad Umer <omerawan445@gmail.com>
2024-03-16 01:15:56 +0500

    Fetched AnalysisAgainstThresholds dynamically

--- src/app/services/fetchSections.js
+++ src/app/services/fetchSections.js
@@ -2,4 +2,4 @@ export const fetchSections = async () => {
     const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + '/api/collections/sections/records');
     const data = await res.json();
     return data;
-  }
+ }
--- src/components/InspectionModal/AnalysisTable.jsx
+++ src/components/InspectionModal/AnalysisTable.jsx
@@ -6,24 +6,27 @@ const AnalysisTable = ({ tableData }) => {
     <div>
       <h6 className="text-uppercase fw-bold mb-3">RAIL PROFILE WEAR</h6>
       <h3 className="fw-fw-lighter">Analysis against Thresholds</h3>
+      <div class="table-wrapper-scroll-y my-custom-scrollbar">
+
       <Table className='custom-table'>
         <thead>
           <tr>
             <th>#</th>
-            <th>Left</th>
-            <th>Right</th>
+            <th>Horizontal</th>
+            <th>Vertical</th>
           </tr>
         </thead>
         <tbody>
           {tableData.map((row, index) => (
             <tr key={index}>
-              <td>{row.direction}</td>
-              <td>{row.left}</td>
-              <td>{row.right}</td>
+              <td>{index + 1}</td>
+              <td>{row.horizontal}</td>
+              <td>{row.vertical}</td>
             </tr>
           ))}
         </tbody>
       </Table>
+      </div>
     </div>
   );
 };
